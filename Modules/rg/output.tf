8ab873f819c62fc41f036771a0756c6d5367bf36
lordbasel <bbasel1513@gmail.com>
2021-07-30 13:01:42 -0400

    CheckPoint #2

---
+++ Modules/rg/output.tf
@@ -0,0 +1,11 @@
+output "rg_name" {
+  value = azurerm_resource_group.rgm.name
+}
+
+output "rg_location" {
+  value = azurerm_resource_group.rgm.location
+}
+
+output "rg_tag" {
+  value = azurerm_resource_group.rgm.tags
+}
